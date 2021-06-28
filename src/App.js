38136d3db3d1f3fecf9b9b7e45f9d9801aacdf18
Maxence <raingeonneau.m@gmail.com>
2021-06-28 12:21:28 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from './views/Home.jsx';
+import Login from './views/Login.jsx';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login"> 
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
