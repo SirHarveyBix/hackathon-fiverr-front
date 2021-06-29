cd1509dd62bef6c45da45e048f998fbdd83b33ec
GUIGANTONDamien <guigantondamien@gmail.com>
2021-06-30 00:56:22 +0200

    Reprise des forminput pour restauration côté back

--- src/views/Event.jsx
+++ src/views/Event.jsx
@@ -6,7 +6,7 @@ import './event.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
 
 function Event() {
   const [users, setUsers] = useState([]);
@@ -38,92 +38,174 @@ function Event() {
   return (
     <div className='container-card-event'>
       <Card className='container-event'>
-        <Card.Body>
-          <Form.Group
-            controlId='exampleForm.ControlSelect2'
-            onSubmit={handleSubmit}
-          >
+        <form onSubmit={handleSubmit}>
+          <div className='inputContent'>
             <Form.Label className='titre-label'>Work with.</Form.Label>
-            <div className='inputContent'>
-              <Form.Control as='select' name='user_id' id='user_id'>
-                {users.map((user) => {
-                  return (
-                    <option key={user.id} name='user_id' value={user.id}>
-                      {user.firstname} {user.lastname}
-                    </option>
-                  );
-                })}
-              </Form.Control>
-              <Form.Group controlId='exampleForm.ControlInput1'>
-                <Form.Label className='titre-label'>About</Form.Label>
-                <Form.Label
-                  label='Titre'
-                  name='title'
-                  type='text'
-                  value={formContent}
-                  setValue={setFormContent}
-                ></Form.Label>
-                <Form.Control type='text' placeholder='Projet x' />
-              </Form.Group>
-
-              <Form.Group controlId='exampleForm.ControlTextarea1'>
-                <Form.Label className='titre-label'>
-                  Motivation / Description
-                </Form.Label>
-                <Form.Label
-                  label='description'
-                  name='description'
-                  type='text'
-                  value={formContent}
-                  setValue={setFormContent}
-                ></Form.Label>
-                <Form.Control as='textarea' rows={3} />
-              </Form.Group>
-              <Button type='submit' variant='outline-success'>Créer l'évenement</Button>
-            </div>
-          </Form.Group>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Body>
-          <Form.Group>
-            <div className='map-box'>
-              <MapContainer
-                center={[47.2074, -1.5556]}
-                zoom={13}
-                scrollWheelZoom={true}
-              >
-                <TileLayer
-                  attribution=''
-                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                />
-                <Marker position={[47.2074, -1.5556]}>
-                  <Popup>
-                    Reserve a desk :<br />{' '}
-                    <FormInput
-                      label='date '
-                      name='date'
-                      type='date'
-                      value={formContent}
-                      setValue={setFormContent}
-                    />
-                    <FormInput
-                      label='time '
-                      name='time'
-                      type='time'
-                      value={formContent}
-                      setValue={setFormContent}
-                    />
-                  </Popup>
-                </Marker>
-              </MapContainer>
+            <select className='label' name='user_id' id='user_id'>
+              {users.map((user) => {
+                return (
+                  <option key={user.id} name='user_id' value={user.id}>
+                    {user.firstname} {user.lastname}
+                  </option>
+                );
+              })}
+            </select>
+            <Form.Label className='titre-label'>About</Form.Label>
+            <FormInput
+              label='Titre'
+              name='title'
+              type='text'
+              value={formContent}
+              setValue={setFormContent}
+            />
+            <Form.Label className='titre-label'>
+              Motivation / Description
+            </Form.Label>
+            <FormControl
+              label='description'
+              name='description'
+              type='text'
+              value={formContent}
+              setValue={setFormContent}
+              as='textarea'
+              rows={6}
+            ></FormControl>
+            <div className="container-button">
               <input type='submit' value="Creer l'evenement" />
             </div>
-          </Form.Group>
-        </Card.Body>
+          </div>
+        </form>
+      </Card>
+      <Card className='container-event'>
+        <form>
+          <div className='map-box'>
+            <MapContainer
+              center={[47.2074, -1.5556]}
+              zoom={13}
+              scrollWheelZoom={true}
+            >
+              <TileLayer
+                attribution=''
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+              />
+              <Marker position={[47.2074, -1.5556]}>
+                <Popup>
+                  Reserve a desk :<br />{' '}
+                  <FormInput
+                    label='date '
+                    name='date'
+                    type='date'
+                    value={formContent}
+                    setValue={setFormContent}
+                  />
+                  <FormInput
+                    label='time '
+                    name='time'
+                    type='time'
+                    value={formContent}
+                    setValue={setFormContent}
+                  />
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </form>
       </Card>
     </div>
   );
 }
+//     <div className='container-card-event'>
+//       <Card className='container-event'>
+//         <Card.Body>
+//           <Form.Group
+//             controlId='exampleForm.ControlSelect2'
+//             onSubmit={handleSubmit}
+//           >
+//             <Form.Label className='titre-label'>Work with.</Form.Label>
+//             <div className='inputContent'>
+//               <Form.Control as='select' name='user_id' id='user_id'>
+//                 {users.map((user) => {
+//                   return (
+//                     <option key={user.id} name='user_id' value={user.id}>
+//                       {user.firstname} {user.lastname}
+//                     </option>
+//                   );
+//                 })}
+//               </Form.Control>
+//               <Form.Group controlId='exampleForm.ControlInput1'>
+//                 <Form.Label className='titre-label'>About</Form.Label>
+//                 <Form.Label
+//                   label='Titre'
+//                   name='title'
+//                   type='text'
+//                   value={formContent}
+//                   setValue={setFormContent}
+//                 ></Form.Label>
+//                 <Form.Control type='text' placeholder='Projet x' />
+//               </Form.Group>
+
+//               <Form.Group controlId='exampleForm.ControlTextarea1'>
+//                 <Form.Label className='titre-label'>
+//                   Motivation / Description
+//                 </Form.Label>
+//                 <FormInput
+//                   className='form-test'
+//                   label='description'
+//                   name='description'
+//                   type='text'
+//                   value={formContent}
+//                   setValue={setFormContent}
+//                   rows={3}
+//                 />
+//                 <Form.Control as='textarea' rows={3} />
+//               </Form.Group>
+//               <Button type='submit' variant='outline-success'>
+//                 Créer l'évenement
+//               </Button>
+//             </div>
+//           </Form.Group>
+//         </Card.Body>
+//       </Card>
+//       <Card>
+//         <Card.Body>
+//           <Form.Group>
+//             <div className='map-box'>
+//               <MapContainer
+//                 center={[47.2074, -1.5556]}
+//                 zoom={13}
+//                 scrollWheelZoom={true}
+//               >
+//                 <TileLayer
+//                   attribution=''
+//                   url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+//                 />
+//                 <Marker position={[47.2074, -1.5556]}>
+//                   <Popup>
+//                     Reserve a desk :<br />{' '}
+//                     <FormInput
+//                       label='date '
+//                       name='date'
+//                       type='date'
+//                       value={formContent}
+//                       setValue={setFormContent}
+//                     />
+//                     <FormInput
+//                       label='time '
+//                       name='time'
+//                       type='time'
+//                       value={formContent}
+//                       setValue={setFormContent}
+//                     />
+//                   </Popup>
+//                   <input type='submit' value="Creer l'evenement" />
+//                 </Marker>
+//               </MapContainer>
+//             </div>
+//           </Form.Group>
+//         </Card.Body>
+//       </Card>
+//     </div>
+//   );
+// }
 
 export default Event;
