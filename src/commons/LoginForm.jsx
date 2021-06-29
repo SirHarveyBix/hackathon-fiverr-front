95172002c49db150323cf7a424718ede947ffc5d
antoniomicalizzi <micalizzi.anto@gmail.com>
2021-06-29 14:27:25 +0200

    loginform en cours

--- src/commons/LoginForm.jsx
+++ src/commons/LoginForm.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  };
+
   return (
     <div>
       <Form style={{ width: '11px', height: '11px' }}>
